a9dd16ed273f06f7d01c6865ad29a8800014160f
Jules Berry <27342552+jules-berry@users.noreply.github.com>
2021-04-14 00:16:21 +0200

    Corrections

--- main5.m
+++ main5.m
@@ -3,31 +3,48 @@
 clear; clc; close all;
 
 p=1; n=6; N=255;
-fcos = @(tet) (tet.^p).*((2*pi.-tet).^p);
-
-%trac� de fcos
+fcos = @(t) ((t.^p).*((2*pi.-t)).^p);
+%trac� de fcos
 figure();
-fplot(fcos, [-2*pi, 2*pi]);
+fplot(fcos, [0, 2*pi]);
 xlabel("tet");
 ylabel("f(cos(tet))");
 title(["Trace de fcos pour p=",num2str(p)]);
-
-res=[];
-resint=[];
-for i=1:N
-  res(i)=scwft(fcos,n,i);
-  resint(i)=sc(fcos,n,i);
+J = 20
+res=zeros(1,J);
+K = 1:J;
+coefs_reels = -2*pi./(K.^2);
+coefs_reels = [2*pi**3/3,coefs_reels];
+%resint=[];
+for i=0:J
+  res(i+1)=scwft(fcos,i,N);
+ % resint(i)=sc(f,i,N);
 endfor
 figure();
-plot(1:N,res);
+hold on;
+plot(0:J,res,"DisplayName","Coefs calculés");
+plot(0:J,coefs_reels,"DisplayName","Coefs réels");
 xlabel("n");
 ylabel("pswft(fcos,n)");
 title("Calcul des produits scalaires");
+legend;
+hold off;
 
 %%% Convergence de la FFT
 
+N = [20:5:200];
+coefs = zeros(1,length(N));
+errs = zeros(1,length(N));
+for i=[1:length(N)]
+  coefs(i) = scwft(fcos,5,N(i));
+  errs(i) = abs(-2*pi/36-coefs(i));
+endfor
 
-
+figure;
+plot(N,errs);
+title("Erreurs");
+xlabel("N");
+ylabel("Erreur");
 
 %%% Temps de calcul compare a une methode d'integration naive
 
--- scwft.m
+++ scwft.m
@@ -2,16 +2,16 @@ function I=scwft(f,n,N)
   tt=linspace(0,2*pi,N);
   g=f(tt).*cos(n*tt);
   
-  fft=fft(g);
+  fourier=fft(g);
   m=length(tt);
-  fft=fft./m;
-  
-  I=2*pi*fft(1);
+  fourier=fourier./m;
+  %display(num2str(m))
+  I=2*pi*fourier(1);
   J=1:m;
   
   for k=1:floor(m/2) %attention peut etre +1
     S = sum(cos(2*pi*k.*J./m));
-    I = I + (2*pi)/m * (fft(k) + fft(floor(m/2) + k)) * S;
+    I = I + (2*pi)/m * (fourier(1+k) + fourier(m - k)) * S;
   endfor
   I = real(I/2);
 endfunction
