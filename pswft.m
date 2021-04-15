63d2abaedfc8e8bf2a31e1ede4d99c7f086532d4
Jules Berry <27342552+jules-berry@users.noreply.github.com>
2021-04-15 14:42:42 +0200

    jupyter

---
+++ pswft.m
@@ -0,0 +1,5 @@
+% Calcule le produit scalaire de f avec le n-ieme polynome de Tchebychev
+% Le parametre N donne le nombre de noeuds de la discretisation de l'intervalle [0,2pi]
+function U = pswft(f,n,N)
+  
+endfunction
