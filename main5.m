15089531a191e5148c99aaa28feb66cef51ca8ec
thomas-poisson <80010421+thomas-poisson@users.noreply.github.com>
2021-04-16 12:55:59 +0200

    optimisation de pswft

--- main5.m
+++ main5.m
@@ -1,72 +1,120 @@
-%%% Script pour l'exercice 5 - transformee de Fourier rapide
+%%% Script pour l'exercice 5 - transformee de Fourier rapide %%%%%%%%%%%%%%%%%%%
 
 clear; clc; close all;
 
-p=1; n=6; N=255;
+%%% Trace de fcos ==============================================================
+p=1;
 fcos = @(t) ((t.^p).*((2*pi.-t)).^p);
-
-%%% Trace de fcos
-
 figure();
-fplot(fcos, [0, 2*pi], "DisplayName", "fcos");
+fplot(fcos, [0 , 2*pi], "DisplayName", "fcos");
 xlabel("tet");
 ylabel("f(cos(tet))");
 title(["Trace de fcos pour p=",num2str(p)]);
-J = 20;
+
+
+%%% Trace des coefficients  ====================================================
+J = 20;  N=85;
 res=zeros(1,J);
 K = 1:J;
 coefs_reels = -2*pi./(K.^2);
 coefs_reels = [2*pi**3/3,coefs_reels];
 %resint=[];
 for i=0:J
-  res(i+1)=scwft(fcos,i,N);
- % resint(i)=sc(f,i,N);
+  res(i+1)=pswft(fcos,N)(i+1);
 endfor
+
 figure();
 hold on;
-plot(0:J,res,"DisplayName","Coefs calculï¿½s");
-plot(0:J,coefs_reels,"DisplayName","Coefs rï¿½els");
+plot(0:J,res,"DisplayName","Coefs calculés");
+plot(0:J,coefs_reels,"DisplayName","Coefs réels");
 xlabel("n");
 ylabel("pswft(fcos,n)");
 title("Calcul des produits scalaires");
 legend;
 hold off;
 
-%%% Convergence de la FFT
 
-N = [20:5:200];
+%%% Convergence de la FFT  =====================================================
+p=1; n=5;
+fcos = @(t) ((t.^p).*((2*pi.-t)).^p);
+N = [20:5:100];
 coefs = zeros(1,length(N));
 errs = zeros(1,length(N));
 for i=[1:length(N)]
-  coefs(i) = scwft(fcos,5,N(i));
-  errs(i) = abs(-2*pi/25-coefs(i))/(2*pi/25);
+  coefs(i) = pswft(fcos,N(i))(n+1);
+  errs(i) = abs(-2*pi/(n^2)-coefs(i))/(2*pi/(n^2));
 endfor
+
+upto=length(N)-3;
+P=polyfit(N(1:upto), log(errs)(1:upto),1);
+disp(["coefficient directeur par interpolation : ",num2str(P(1))]);
 
 figure;
-plot(N,log(errs));
-title("Erreur relative");
+hold on;
+plot(N,[log(errs); P(2).+P(1).*N]);
+legend(["log(err)"; "droite d'interpolation"]);
+title(["Erreur relative pour (p,n)=(", num2str(p),",", num2str(n),")"]);
 xlabel("N");
-ylabel("Erreur");
+ylabel("log(Erreur)");
+hold off;
 
-%%% Temps de calcul compare a une methode d'integration naive
 
-p=4; N=255;
-tft=[];
-tint=[];
-Iint=[];
-Ifft=[];
-for n=0:N
-  fcos = @(tet) (tet.^p).*((2*pi.-tet).^p);
-  ts=cputime;
-  Ifft(n+1)=scwft(fcos,n,N);
-  te=cputime;
-  tft(n+1)=te-ts;
-  fun=@(x) fcos(x).*cos(n.*x);
-  ts=cputime;
-  Iint(n+1)=integral(fun,0,2*pi)/2;
-  te=cputime;
-  tint(n+1)=te-ts;
+%%% Trace de fcos ==============================================================
+p=4;
+fcos = @(t) ((t.^p).*((2*pi.-t)).^p);
+figure();
+hold on;
+fplot(fcos, [0 , 2*pi], "DisplayName", "fcos");
+xlabel("tet");
+ylabel("f(cos(tet))");
+title(["Trace de fcos pour p=",num2str(p)]);
+hold off;
+
+%{
+%%% Convergence de la FFT  =====================================================
+p=4; n=0;
+fcos = @(t) ((t.^p).*((2*pi.-t)).^p);
+N = [20:5:300];
+coefs = zeros(1,length(N));
+errs = zeros(1,length(N));
+
+fun=@(x) fcos(x).*cos(n.*x);
+valex=256*pi/(2*315); %pswft(fcos,85)(n+1); %a remplacer par la valeur exacte
+for i=[1:length(N)]
+  coefs(i) = pswft(fcos,N(i))(n+1);
+  errs(i) = abs((valex-coefs(i))/(valex));
 endfor
 
-disp(["temps total de calcul par transformï¿½e de fourier : ",num2str(sum(tft))," s"]);
-disp(["temps total de calcul par integration numerique : ",num2str(sum(tint))," s"]);
+upto=length(N)-3;
+P=polyfit(N(1:upto), log(errs)(1:upto),1);
+disp(["coefficient directeur par interpolation : ",num2str(P(1))]);
+
+figure;
+plot(N,[log(errs); P(2).+P(1).*N]);
+legend(["log(err)"; "droite d'interpolation"]);
+title(["Erreur relative pour (p,n)=(", num2str(p),",", num2str(n),")"]);
+xlabel("N");
+ylabel("log(Erreur)");
+%}
+
+%%% Temps de calcul compare a une methode d'integration naive ==================
+p=4; N=255;
+Iint=zeros(1,N);
+Ifft=zeros(1,N);
+
+ts=cputime;
+fcos = @(tet) (tet.^p).*((2*pi.-tet).^p);
+Ifft=pswft(fcos,N);
+te=cputime;
+tft=te-ts;
+
+ts=cputime;
+for n=0:N
+  fun=@(x) fcos(x).*cos(n.*x);
+  Iint(n+1)=integral(fun,0,2*pi)/2;
+endfor
+te=cputime;
+tint=te-ts;
+
+disp(["temps total de calcul par transformée de fourier : ",num2str(tft)," s"]);
+disp(["temps total de calcul par integration numerique : ",num2str(tint)," s"]);
